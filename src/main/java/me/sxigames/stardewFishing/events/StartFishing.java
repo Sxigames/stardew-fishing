cfc99ecb0b88461efdffcb4fcbf0bf3594ddf2d9
Jorge Felipe <sxigames63@gmail.com>
2025-06-15 00:06:35 -0300

    Add "fish" to bar

--- src/main/java/me/sxigames/stardewFishing/events/StartFishing.java
+++ src/main/java/me/sxigames/stardewFishing/events/StartFishing.java
@@ -10,9 +10,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class StartFishing implements Listener {
     @EventHandler(ignoreCancelled = true)
@@ -32,28 +35,52 @@ public class StartFishing implements Listener {
                 if (Arrays.stream(fishMaterials).anyMatch(material -> material == caughtItem.getType())) {
                     event.setCancelled(true);
                     player.sendMessage(Component.text("You started fishing! You're getting a: ").append(caughtItem.displayName()));
-                    Component fishBar = Component.text(builder -> {
-                        builder.append(Component.text("["));
-                        for (int i = 0; i < 100; i++) {
-                            if (i < 20) {
-                                builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.GREEN));
-                            } else {
-                                builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.RED));
-                            }
-                        }
-                        builder.append(Component.text("]"));
-                    });
-                    Title title = Title.title(
-                            fishBar,
-                            Component.text("Fishing: ").append(caughtItem.displayName()),
-                            Title.Times.times(Duration.ZERO, Duration.ofSeconds(30), Duration.ZERO)
-                    );
-                    player.showTitle(title);
                     player.addScoreboardTag("fishing");
-                    player.getServer().getScheduler().runTaskLater(StardewFishing.getPlugin(), () -> {
+                    Plugin plugin = StardewFishing.getPlugin();
+                    AtomicInteger fishPos = new AtomicInteger((int) (Math.random() * 100));
+                    AtomicBoolean direction = new AtomicBoolean(true); // true = right, false = left
+                    player.sendMessage(String.valueOf(fishPos.get()));
+                    int taskID = player.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+                        if (player.getScoreboardTags().contains("fishing")) {
+                            if(fishPos.get() >= 100){
+                                direction.set(false); // Change direction to left
+                            }
+                            if (fishPos.get() <= 0) {
+                                direction.set(true); // Change direction to right
+                            }
+                            if (direction.get()) {
+                                fishPos.addAndGet(1); // Move right
+                            } else {
+                                fishPos.addAndGet(-1); // Move left
+                            }
+                            Component fishBar = Component.text(builder -> {
+                                builder.append(Component.text("["));
+                                for (int i = 0; i < 100; i++) {
+                                    if (i == fishPos.get()) {
+                                        builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.RED));
+                                    }
+                                    else if (i < 20) {
+                                        builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.YELLOW));
+                                    } else {
+                                        builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.BLUE));
+                                    }
+
+                                }
+                                builder.append(Component.text("]"));
+                            });
+                            Title title = Title.title(
+                                    fishBar,
+                                    Component.text("Fishing: ").append(caughtItem.displayName()),
+                                    Title.Times.times(Duration.ZERO, Duration.ofSeconds(1), Duration.ZERO)
+                            );
+                            player.showTitle(title);
+                        }
+                    }, 0L, 1L); // 1 tick = 20 times per second
+                    player.getServer().getScheduler().runTaskLater(plugin, () -> {
                         player.removeScoreboardTag("fishing");
                         player.sendMessage(Component.text("You stopped fishing!"));
                         event.getHook().remove();
+                        player.getServer().getScheduler().cancelTask(taskID);
                     }, 600L); // 600 ticks = 30 seconds
 
                 } else {
