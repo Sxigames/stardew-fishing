8624e29eb66ba776f628e5d292e61cef66f4d22d
Jorge Felipe <sxigames63@gmail.com>
2025-06-17 00:04:14 -0300

    Allow player to move bar

--- src/main/java/me/sxigames/stardewFishing/events/StartFishing.java
+++ src/main/java/me/sxigames/stardewFishing/events/StartFishing.java
@@ -4,12 +4,14 @@ import me.sxigames.stardewFishing.StardewFishing;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 
 import java.time.Duration;
@@ -21,8 +23,16 @@ public class StartFishing implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerFish(PlayerFishEvent event) {
         Player player = event.getPlayer();
+        Plugin plugin = StardewFishing.getPlugin();
+        NamespacedKey barLocationKey = new NamespacedKey(plugin, "barLocation");
+//        NamespacedKey barSpeedKey = new NamespacedKey(plugin, "barSpeed");
+        player.sendMessage(event.getState().name());
         switch (event.getState()) {
             case CAUGHT_FISH -> {
+                if (player.getScoreboardTags().contains("fishing")) {
+                    event.setCancelled(true);
+                    return;
+                }
                 Item caughtEntity = (Item) event.getCaught();
                 assert caughtEntity != null;
                 ItemStack caughtItem = caughtEntity.getItemStack();
@@ -36,37 +46,42 @@ public class StartFishing implements Listener {
                     event.setCancelled(true);
                     player.sendMessage(Component.text("You started fishing! You're getting a: ").append(caughtItem.displayName()));
                     player.addScoreboardTag("fishing");
-                    Plugin plugin = StardewFishing.getPlugin();
                     AtomicInteger fishPos = new AtomicInteger((int) (Math.random() * 100));
-                    AtomicBoolean direction = new AtomicBoolean(true); // true = right, false = left
+                    AtomicBoolean fishDirection = new AtomicBoolean(true); // true = right, false = left
                     player.sendMessage(String.valueOf(fishPos.get()));
+                    player.getPersistentDataContainer().set(barLocationKey, PersistentDataType.INTEGER, 0);
                     int taskID = player.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
-                        if (player.getScoreboardTags().contains("fishing")) {
                             if(fishPos.get() >= 100){
-                                direction.set(false); // Change direction to left
+                                fishDirection.set(false); // Change fishDirection to left
                             }
                             if (fishPos.get() <= 0) {
-                                direction.set(true); // Change direction to right
+                                fishDirection.set(true); // Change fishDirection to right
                             }
-                            if (direction.get()) {
+                            if (fishDirection.get()) {
                                 fishPos.addAndGet(1); // Move right
                             } else {
                                 fishPos.addAndGet(-1); // Move left
                             }
                             Component fishBar = Component.text(builder -> {
                                 builder.append(Component.text("["));
+                                Integer barLocation = player.getPersistentDataContainer().get(barLocationKey, PersistentDataType.INTEGER);
+                                if (barLocation == null) {
+                                    barLocation = 0; // Default value
+                                }
                                 for (int i = 0; i < 100; i++) {
                                     if (i == fishPos.get()) {
                                         builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.RED));
                                     }
-                                    else if (i < 20) {
+                                    else if (i < barLocation + 10 && i > barLocation) {
                                         builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.YELLOW));
                                     } else {
                                         builder.append(Component.text("▏", net.kyori.adventure.text.format.NamedTextColor.BLUE));
                                     }
-
                                 }
                                 builder.append(Component.text("]"));
+                                if(barLocation > 0) {
+                                    player.getPersistentDataContainer().set(barLocationKey, PersistentDataType.INTEGER, barLocation - 1);
+                                }
                             });
                             Title title = Title.title(
                                     fishBar,
@@ -74,8 +89,7 @@ public class StartFishing implements Listener {
                                     Title.Times.times(Duration.ZERO, Duration.ofSeconds(1), Duration.ZERO)
                             );
                             player.showTitle(title);
-                        }
-                    }, 0L, 1L); // 1 tick = 20 times per second
+                    }, 0L, 2L); // 2 ticks = 10 times per second
                     player.getServer().getScheduler().runTaskLater(plugin, () -> {
                         player.removeScoreboardTag("fishing");
                         player.sendMessage(Component.text("You stopped fishing!"));
@@ -88,13 +102,23 @@ public class StartFishing implements Listener {
                 }
             }
 
-            case REEL_IN, BITE -> {
+            case REEL_IN -> {
                 if (player.getScoreboardTags().contains("fishing")) {
                     event.setCancelled(true);
+                    Integer barLocation = player.getPersistentDataContainer().get(barLocationKey, PersistentDataType.INTEGER);
+//                    Integer barSpeed = player.getPersistentDataContainer().get(barSpeedKey, PersistentDataType.INTEGER);
+                    if (barLocation == null) {
+                        barLocation = 0; // Default value
+                    }
+                    if (barLocation <= 90){
+                        player.getPersistentDataContainer().set(barLocationKey, PersistentDataType.INTEGER, barLocation + 2);
+                    }
                 }
             }
             default -> {
-
+                if (player.getScoreboardTags().contains("fishing")) {
+                    event.setCancelled(true);
+                }
             }
         }
 
